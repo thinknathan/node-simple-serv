de58765d35391b99a4cba283ed4d06eeebe6ac8a
thinknathan <github@thinknathan.com>
2023-12-29 15:37:43 -0800

    Fix

--- serve.cjs
+++ serve.cjs
@@ -2,54 +2,55 @@
 'use strict';
 const http = require('http');
 const path = require('path');
-const mime = import('mime');
+const mime = require('mime-types');
 const fs = require('fs');
 const yargs = require('yargs');
 
-const root = process.cwd();
+function main() {
+	const root = process.cwd();
 
-const argv = yargs
-	.option('port', {
-		alias: 'p',
-		description: 'Port number for the server',
-		default: 8080,
-	})
-	.option('index-extension', {
-		alias: 'i',
-		description: 'Extension for the index file',
-		default: 'html',
-	}).argv;
+	const argv = yargs
+		.option('port', {
+			alias: 'p',
+			description: 'Port number for the server',
+			default: 8080,
+		})
+		.option('index-extension', {
+			alias: 'i',
+			description: 'Extension for the index file',
+			default: 'html',
+		}).argv;
 
-const port = argv.port;
-const indexExtension = argv['index-extension'];
+	const port = argv.port;
+	const indexExtension = argv['index-extension'];
 
-const server = http.createServer((req, res) => {
-	const filePath = path.join(root, req.url);
+	const server = http.createServer((req, res) => {
+		const filePath = path.join(root, req.url);
 
-	if (!fs.existsSync(filePath)) {
-		res.writeHead(404, { 'Content-Type': 'text/plain' });
-		res.end('Not Found');
-		return;
-	}
+		if (!fs.existsSync(filePath)) {
+			res.writeHead(404, { 'Content-Type': 'text/plain' });
+			res.end('Not Found');
+			return;
+		}
 
-	const isDirectory = fs.statSync(filePath).isDirectory();
+		const isDirectory = fs.statSync(filePath).isDirectory();
 
-	if (isDirectory) {
-		const indexFileName = `index.${indexExtension}`;
-		const indexFilePath = path.join(filePath, indexFileName);
+		if (isDirectory) {
+			const indexFileName = `index.${indexExtension}`;
+			const indexFilePath = path.join(filePath, indexFileName);
 
-		if (fs.existsSync(indexFilePath)) {
-			// Serve the specified index file if it exists in the directory
-			const fileStream = fs.createReadStream(indexFilePath);
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			fileStream.pipe(res);
-		} else {
-			// Serve directory listing as HTML
-			const directoryListing = fs
-				.readdirSync(filePath)
-				.map((item) => `<li>${item}</li>`)
-				.join('');
-			const html = `
+			if (fs.existsSync(indexFilePath)) {
+				// Serve the specified index file if it exists in the directory
+				const fileStream = fs.createReadStream(indexFilePath);
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				fileStream.pipe(res);
+			} else {
+				// Serve directory listing as HTML
+				const directoryListing = fs
+					.readdirSync(filePath)
+					.map((item) => `<li>${item}</li>`)
+					.join('');
+				const html = `
         <!DOCTYPE html>
         <html>
           <head>
@@ -61,23 +62,26 @@ const server = http.createServer((req, res) => {
           </body>
         </html>
       `;
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end(html);
-		}
-	} else {
-		if (path.extname(filePath) === '.wasm') {
-			const wasmMimeType = mime.getType('wasm');
-			res.writeHead(200, { 'Content-Type': wasmMimeType });
-			const fileStream = fs.createReadStream(filePath);
-			fileStream.pipe(res);
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.end(html);
+			}
 		} else {
-			// Serve other files
-			const fileStream = fs.createReadStream(filePath);
-			fileStream.pipe(res);
+			if (path.extname(filePath) === '.wasm') {
+				const wasmMimeType = mime.contentType('wasm');
+				res.writeHead(200, { 'Content-Type': wasmMimeType });
+				const fileStream = fs.createReadStream(filePath);
+				fileStream.pipe(res);
+			} else {
+				// Serve other files
+				const fileStream = fs.createReadStream(filePath);
+				fileStream.pipe(res);
+			}
 		}
-	}
-});
+	});
 
-server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}/`);
-});
+	server.listen(port, () => {
+		console.log(`Server running at http://localhost:${port}/`);
+	});
+}
+
+main();
